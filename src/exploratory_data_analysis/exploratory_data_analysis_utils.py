f7976a03274a650ff79e594d5216fe78f9856128
Simone Porreca <porrecasimone@gmail.com>
2024-06-16 19:48:49 +0200

    Refactor plot_time_series

--- src/exploratory_data_analysis/exploratory_data_analysis_utils.py
+++ src/exploratory_data_analysis/exploratory_data_analysis_utils.py
@@ -1,6 +1,7 @@
 """
 The module contains several util functions for performing exploratory data analysis.
 """
+import matplotlib.pyplot as plt
 # Import Standard Libraries
 import numpy as np
 import os
@@ -58,7 +59,7 @@ def plot_time_series(time_series: pd.DataFrame,
                      x_column: str,
                      y_column: str,
                      title: str,
-                     labels: Tuple[str, str]) -> matplotlib.axes.Axes:
+                     labels: Tuple[str, str, str]) -> matplotlib.axes.Axes:
     """
     Plots a time series using seaborn matplotlib Axes.
 
@@ -67,7 +68,7 @@ def plot_time_series(time_series: pd.DataFrame,
         x_column: String name of column in time_series for x-axis
         y_column: String name of column in time_series for y-axis
         title: String title of plot
-        labels: Tuple of two strings containing labels for x-axis and y-axis
+        labels: Tuple of three strings containing labels for x-axis and y-axis and for the plot
 
     Returns:
         ax: matplotlib Axes
@@ -75,12 +76,14 @@ def plot_time_series(time_series: pd.DataFrame,
     # Plot the data
     ax = sns.lineplot(data=time_series,
                       x=time_series[x_column],
-                      y=time_series[y_column])
+                      y=time_series[y_column],
+                      label=labels[2])
 
     # Set title
     ax.set_title(title,
                  fontweight='bold',
                  fontsize=20)
+
     # Set tick rotation
     ax.tick_params(labelrotation=45)
 
@@ -89,6 +92,18 @@ def plot_time_series(time_series: pd.DataFrame,
                   fontsize=14)
     ax.set_ylabel(labels[1],
                   fontsize=14)
+
+    # Set the legend
+    ax.legend(loc='upper center',
+              bbox_to_anchor=(0.5, 0.95),
+              fontsize=12,
+              ncol=2)
+
+    # Show plt
+    plt.show()
+
+    # Define the layout
+    plt.tight_layout()
 
     return ax
 
@@ -99,17 +114,19 @@ def plot_predictions_vs_time_series(time_series: pd.DataFrame,
                                     y_column: str,
                                     title: str,
                                     labels: Tuple[str, str]) -> matplotlib.axes.Axes:
-
     # Retrieve time series plot
-    ax = plot_time_series(time_series=time_series,
-                          x_column=x_column,
-                          y_column=y_column,
-                          title=title,
-                          labels=labels)
+    ax_time_series = plot_time_series(time_series=time_series,
+                                      x_column=x_column,
+                                      y_column=y_column,
+                                      title=title,
+                                      labels=labels)
 
     # Plot predictions
-    sns.lineplot(x=time_series[x_column],
-                 y=predictions,
-                 ax=ax)
+    ax_predictions = sns.lineplot(x=time_series[x_column],
+                                  y=predictions,
+                                  ax=ax_time_series)
 
-    return ax
+    ax_predictions.legend(handles=[ax_time_series, ax_predictions],
+                          labels=['Times Series', 'Predictions'])
+
+    return ax_predictions
